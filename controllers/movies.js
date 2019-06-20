8c8f5140cf67a1800f167e086701c50c65e53a89
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-06-19 22:38:52 -0700

    Users can see movie pages and trailers

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,9 +4,22 @@ require('dotenv').config();
 
 const moviedb = new MovieDb(process.env.movieDB_APIKEY);
 module.exports = (app) => {
+  // home
   app.get('/', (req, res) => {
     moviedb.miscNowPlayingMovies().then((response) => {
       res.render('movies-index', { movies: response.results });
+    }).catch(console.error);
+  });
+
+  app.get('/movies/:id', (req, res) => {
+    moviedb.movieInfo({ id: req.params.id }).then((movie) => {
+      moviedb.movieTrailers({ id: req.params.id }).then((videos) => {
+        const theMovie = movie;
+        theMovie.trailer_youtube_id = videos.youtube[0].source;
+        console.log('VIDEOS.TRAILER_YOUTUBE_ID', movie.trailer_youtube_id);
+
+        res.render('movies-show', { movie });
+      }).catch(console.error);
     }).catch(console.error);
   });
 };

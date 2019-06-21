8666487dfa444867d7e089a5846ddcbefdfd0159
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-06-20 17:31:24 -0700

    minor fix

--- controllers/movies.js
+++ controllers/movies.js
@@ -12,26 +12,12 @@ module.exports = (app) => {
     }).catch(console.error);
   });
 
-
-  app.get('/movies/:id', (req, res) => {
-    moviedb.movieInfo({ id: req.params.id }).then((movie) => {
-      moviedb.movieTrailers({ id: req.params.id }).then((videos) => {
-        const theMovie = movie;
-        theMovie.trailer_youtube_id = videos.youtube[0].source;
-        console.log('VIDEOS.TRAILER_YOUTUBE_ID', movie.trailer_youtube_id);
-
-        res.render('movies-show', { movie });
-      });
-    });
-  });
-
   // SHOW
   app.get('/movies/:id', (req, res) => {
     moviedb.movieInfo({ id: req.params.id }).then((movie) => {
       moviedb.movieTrailers({ id: req.params.id }).then((videos) => {
         const mov = movie;
         mov.trailer_youtube_id = videos.youtube[0].source;
-        console.log('VIDEOS.TRAILER_YOUTUBE_ID', movie.trailer_youtube_id);
         // FIND THIS MOVIE'S REVIEWS
         Review.find({ movieId: req.params.id }).then((reviews) => {
           // THEN RENDER THE MOVIES-SHOW TEMPLATE
